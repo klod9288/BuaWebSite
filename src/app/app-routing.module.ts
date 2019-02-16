82559f5e3042010be36b46f43cc68468acf94a1f
srongklod <klod9288@gmail.com>
2019-02-16 13:38:42 +0700

    Router Success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SinginComponent } from './singin/singin.component';
+import { SingupComponent } from './singup/singup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SinginComponent },
+  { path: 'signup', component: SingupComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SinginComponent } from './singin/singin.component';
+import { SingupComponent } from './singup/singup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
