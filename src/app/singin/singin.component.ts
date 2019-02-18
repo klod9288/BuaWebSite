2f123671d4c245eddf2a7af6cb0ba4d4dce406be
srongklod <klod9288@gmail.com>
2019-02-18 13:25:22 +0700

    onClick Submit

--- src/app/singin/singin.component.ts
+++ src/app/singin/singin.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SinginComponent implements OnInit {
 
+  private email: String;
+  private password: String;
+
   constructor() { }
 
   ngOnInit() {
+    this.email = '';
+    this.password = '';
+  }
+
+  onClickSubmit() {
+    console.log('You Click Submit');
   }
 
 }
